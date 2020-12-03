a3df55b43a7b4bec30ec45757612f0d75ed8b1a5
mhz91 <49787689+mhz91@users.noreply.github.com>
2020-12-03 14:34:54 +0100

    Add files via upload

---
+++ Brooklyn-Edu-Test/footer.php
@@ -0,0 +1,38 @@
+<footer class="footer-inner">
+        
+        <div class="footer-row1 container">
+            <img class="img-footer" src="./image/logo-brooklyn-footer.svg" alt="">
+        </div>
+
+        <div class="footer-row2 container">
+            <div class="footer-rgpd ">
+            <div class="menu-legal-menu">
+            <nav class="menu-legal">
+                <ul>
+                    <li>© 2004 – 2020 The Brooklyn Brewery, Brooklyn NY.</li>
+                    <li>Terms and Conditions</li>
+                    <li>Privacy Policy</li>
+                    <li>CA Consumer Protection</li>
+                </ul>
+            </nav>
+            </div>
+        </div>
+        
+            <div class="footer-rs ">
+            <nav class="social-nav">
+                <ul>
+                    <li><a href="https://www.facebook.com/thebrooklynbrewery" class="rs" target="_blank" id="icon-footer"><i
+                        class="fab fa-facebook-f fa-2x"></i></a></li>
+                    <li><a href="https://twitter.com/brooklynbrewery" class="rs" target="_blank" id="icon-footer"><i
+                        class="fab fa-twitter fa-2x"></i></a></li>
+                    <li><a href="https://www.instagram.com/brooklynbrewery/" class="rs" target="_blank" id="icon-footer"><i
+                        class="fab fa-instagram fa-2x" ></i></a></li>
+                    <li><a href="https://www.flickr.com/people/brooklynbrewery/" class="rs" target="_blank" id="icon-footer"><i
+                        class="fab fa-flickr fa-2x"></i></a></li>
+                    <li><a href="https://www.youtube.com/user/TheBrooklynBrewery" class="rs" target="_blank" id="icon-footer"><i
+                        class="fab fa-youtube fa-2x"></i></a></li>
+                </ul>
+</nav>
+            </div>
+        </div>
+    </footer>
